b587a691ca468c3593d5316040768c35161aa846
JohannesVora <wi23b139@technikum-wien.at>
2025-05-12 10:59:47 +0200

    - splitted script.js

---
+++ Frontend/js/orders.js
@@ -0,0 +1,85 @@
+function loadOrders() {
+    $.ajax({
+      url: "http://localhost/Zeitwert/Backend/logic/requestHandler.php",
+      type: "POST",
+      data: {
+        action: "loadOrders"
+      },
+      dataType: "json",
+      success: function (response) {
+        if (response.success) {
+          const order = response.orders;
+          const $tableBody = $("#orderTable");
+          $tableBody.empty();
+  
+          if (Object.keys(order).length === 0) {
+            $("#noOrders").text("Es schaut so aus, als ob du noch keine Bestellungen bei Zeitwert getätigt hast.");
+            $("#order-count").text("0");
+            return;
+          }
+  
+          $.each(order, function (orderId, item) {
+  
+            const $tableRow = $(`
+              <tr>
+                <th scope="row">${item.orderId}</th>
+                <td>${item.total_price}</td>
+                <td>${item.order_date}</td>
+                <td>${item.status}</td>
+                <td>
+                  <button class="btn btn-secondary btn-sm mt-2" onclick="loadOrderItems(${item.orderId})">Details</button>
+                </td>
+              </tr>
+            `);
+  
+            $tableBody.append($tableRow);
+          });
+        }
+      },
+      error: function (xhr, status, error) {
+        console.error("Fehler bei der Suche:", error);
+      }
+    });
+}
+  
+  function loadOrderItems(orderId) {
+    $.ajax({
+      url: "http://localhost/Zeitwert/Backend/logic/requestHandler.php",
+      type: "POST",
+      data: {
+        action: "loadOrderItems",
+        orderId: orderId
+      },
+      dataType: "json",
+      success: function (response) {
+        if (response.success) {
+          const items = response.items;
+          const $tbody = $("#orderDetailsBody");
+          $tbody.empty();
+  
+          $.each(items, function (index, item) {
+            const row = `
+                          <tr>
+                            <td>${item.produktname}</td>
+                            <td>${item.menge}</td>
+                            <td>${parseFloat(item.preis).toFixed(2)} €</td>
+                            <td>${(item.menge * item.preis).toFixed(2)} €</td>
+                          </tr>
+                        `;
+  
+            $tbody.append(row);
+          });
+  
+          // Modal anzeigen
+          const modal = new bootstrap.Modal($('#orderDetailsModal'));
+          modal.show();
+          //$tbody.removeClass("d-none");
+        } else {
+          alert("Fehler beim Laden der Bestelldetails.");
+        }
+      },
+      error: function (xhr, status, error) {
+        console.error("Fehler bei der Anfrage:", error);
+      }
+    });
+}
